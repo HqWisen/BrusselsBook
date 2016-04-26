ba0c2aabed720d06938c3ef8c8f54d06bb147319
Hakim Boulahya <hakimblhya@gmail.com>
2016-04-27 01:15:47 +0200

    finishing script sql (still HalfDaysOff problem)

--- script/brusselsBook.sql
+++ script/brusselsBook.sql
@@ -1,156 +1,164 @@
-use brusselsbook;
 
-create table Establishment(
-	EID int(),
-	EName varchar(),
-	Latitude double(),
-	Longitude double(),
-	PhoneNumber varchar(),
-	Modified bool(),
-	Website varchar(), #NULL
-    primary key(EID)
-)
+# Create a user too access to brusselsbook database
+CREATE USER 'bbadmin'@'localhost' IDENTIFIED BY 'common';
 
-create table Restaurant(
-	EID int() ,
-	PriceMinimum int(),
-	PriceMaximum int(),
-	HasTakeaway bool(),
-	MakeDelivry bool(),
-	HalfDaysOff array(),
-	foreign key (EID) references Establishment(EID)         
-)
+# Create the database
+CREATE DATABASE brusselsbook;
 
-	
-)
+# Give access to the user
+GRANT ALL PRIVILEGES ON `brusselsbook`.* TO 'bbadmin'@'localhost';
 
-create table Bar(
-	EID int(),
-	CanSmoke bool(),
-	MakeRestoration bool(),
-	foreign key (EID) references Establishment(EID)         
+# Use the new created database
+USE brusselsbook;
 
-)
+CREATE TABLE Establishment(
+	EID INT NOT NULL AUTO_INCREMENT,
+	EName VARCHAR(20) NOT NULL,
+	Latitude FLOAT(10, 4) NOT NULL,
+	Longitude FLOAT(10, 4) NOT NULL,
+	PhoneNumber VARCHAR(20) NOT NULL,
+	Modified TINYINT(1) DEFAULT 0,
+	Website VARCHAR(50),
+	PRIMARY KEY(EID)
+);
 
-create table Hotel(
-	EID int(),
-	NoStars int(),
-	NoRooms int(),
-	PriceForTwo int(),
-	foreign key (EID) references Establishment(EID)         
+CREATE TABLE Restaurant(
+	EID INT,
+	PriceMinimum INT NOT NULL,
+	PriceMaximum INT NOT NULL,
+	HasTakeaway TINYINT(1) NOT NULL,
+	MakeDelivry TINYINT(1) NOT NULL,
+	HalfDaysOff ARRAY(),
+	FOREIGN KEY (EID) REFERENCES Establishment(EID)
+);
 
-)
+CREATE TABLE Bar(
+	EID INT,
+	CanSmoke TINYINT(1) NOT NULL,
+	MakeRestoration TINYINT(1) NOT NULL,
+	FOREIGN KEY (EID) REFERENCES Establishment(EID)
+);
 
-create table Address(
-	EID int(),
-	Street varchar(),
-	StreetNumber int (), 
-	Locality varchar (),
-	PostalCode double()          
-	foreign key (EID) references Establishment(EID)         
+CREATE TABLE Hotel(
+	EID INT,
+	NoStars SMALLINT NOT NULL,
+	NoRooms SMALLINT NOT NULL,
+	PriceForTwo FLOAT(10, 2) NOT NULL,
+	FOREIGN KEY (EID) REFERENCES Establishment(EID)
+);
 
-)
+CREATE TABLE Address(
+	EID INT,
+	Street VARCHAR(50) NOT NULL,
+	StreetNumber SMALLINT NOT NULL, 
+	Locality VARCHAR(50) NOT NULL,
+	PostalCode SMALLINT NOT NULL,
+	FOREIGN KEY (EID) REFERENCES Establishment(EID)
+);
 
-create table BookUser(
-	UID int(),
-	EmailAdress varchar() unique,
-	Username varchar() unique,
-	Pwd varchar(),
-	RegistrationDate date(), #varchar  , peut importe son heure d'enrigstration 
-    primary key(UID)
-)
+CREATE TABLE BookUser(
+	UID INT NOT NULL AUTO_INCREMENT,
+	EmailAdress VARCHAR(50) NOT NULL UNIQUE KEY,
+	Username VARCHAR(15) NOT NULL UNIQUE KEY,
+	Pwd VARCHAR(16) NOT NULL,
+	RegistrationDate DATE NOT NULL, 
+    PRIMARY KEY(UID)
+);
 
-create table Administrator(
-	AID int(),
-	UID int(),
-    primary key(AID),
-	foreign key (UID) references BookUser(UID)         
+CREATE TABLE Administrator(
+	AID INT NOT NULL AUTO_INCREMENT,
+	UID INT,
+    PRIMARY KEY(AID),
+	FOREIGN KEY (UID) REFERENCES BookUser(UID)
+);
 
-)
+# Even if Describer is not in the relationnal model,
+# we choose to create this table (less difficult to manage the modifications)
+CREATE TABLE Describer(
+	DID INT NOT NULL AUTO_INCREMENT,
+    Modified TINYINT(1) DEFAULT 0,
+    PRIMARY KEY(DID)
+);
 
-create table EstablishmentModification(
-	OldEID int(),
-	NewEID int(),
-	AID int(),
-	ModificationDate datetime(),# varchar  , le temps est important ; on peut éviter les secondes 
-	foreign key (OldEID) references Establishment(EID),        
-	foreign key (NewEID) references Establishment(EID),
-	foreign key (AID) references Administrator(AID)         
-)
+CREATE TABLE BookComment(
+	DID INT,
+    UID INT,
+    EID INT,
+	CreationDate DATETIME NOT NULL,
+    Score TINYINT NOT NULL,
+	BookText TEXT NOT NULL,
+    UNIQUE KEY(DID, UID, CreationDate),
+	FOREIGN KEY (DID) REFERENCES Describer(DID),
+	FOREIGN KEY (UID) REFERENCES BookUser(UID),
+    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+);
 
-create table EstablishmentDeletion(
-	EID int(),
-	AID int(),
-	DeletionDate datetime(),#varchar, on pourrait éviter un jeu ou un suprrime et l'autre ajoute , laisser un laps de temps ....
-	foreign key (EID) references Establishment(EID),
-	foreign key (AID) references Administrator(AID)         
+CREATE TABLE Tag(
+	DID INT,
+	TagName VARCHAR(20) NOT NULL UNIQUE KEY,
+    UID INT,
+    CreationDate DATE NOT NULL, 
+	FOREIGN KEY (DID) REFERENCES Describer(DID),
+	FOREIGN KEY (UID) REFERENCES BookUser(UID)
+);
 
-)
+CREATE TABLE TagDescribe(
+	TagName VARCHAR(20),
+	EID INT,
+	UID INT,
+    FOREIGN KEY (TagName) REFERENCES Tag(TagName),
+	FOREIGN KEY (EID) REFERENCES Establishment(EID),     
+	FOREIGN KEY (UID) REFERENCES BookUser(UID)
+);
 
-create table UserDeletion(
-	EID int(),
-	AID int(),
-	DeletionDate datetime()	#varchar
-    foreign key (EID) references Establishment(EID),
-	foreign key (AID) references Administrator(AID)     
-)
+CREATE TABLE UserSignal(
+	DID INT,
+	SignalerUID INT,
+ 	FOREIGN KEY (DID) REFERENCES Describer(DID),    
+	FOREIGN KEY (SignalerUID) REFERENCES BookUser(UID)       
+);
 
-create table DescriberDeletion(
-	DID int(),
-	UID int(),
-	DeletionDate datetime(),
-    foreign key (DID) references Establishment(EID),
-	foreign key (AID) references Administrator(AID)   
-)
+CREATE TABLE EstablishmentModification(
+	OldEID INT,
+	NewEID INT,
+	AID INT,
+	ModificationDate DATETIME NOT NULL,
+	FOREIGN KEY (OldEID) REFERENCES Establishment(EID),
+	FOREIGN KEY (NewEID) REFERENCES Establishment(EID),
+	FOREIGN KEY (AID) REFERENCES Administrator(AID)
+);
 
-create table DescriberModification(
-	OldDID int(),
-	NewDID int(),
-	UID int(),
-	ModificationDate datetime()
-	foreign key (EID) references Establishment(EID),       
-	foreign key (AID) references Administrator(AID)    
-)
+CREATE TABLE EstablishmentDeletion(
+	EID INT,
+	AID INT,
+	DeletionDate DATETIME NOT NULL,
+	FOREIGN KEY (EID) REFERENCES Establishment(EID),
+	FOREIGN KEY (AID) REFERENCES Administrator(AID)
+);
 
-create table BookComment(
-	DID int(),
-	UID int(),
-	EID int(),
-	CreationDate datetime(),
-	Score int(),
-	BookText text(), #string , ne pas limiter la taille du text .
-	Modified bool(),
-    primary key(DID),
-	foreign key (UID) references BookUser(UID),      
-	foreign key (EID) references Establishment(EID)    
-)
+CREATE TABLE UserDeletion(
+	UID INT,
+	AID INT,
+	DeletionDate DATETIME NOT NULL,
+    FOREIGN KEY (EID) REFERENCES BookUser(UID),
+	FOREIGN KEY (AID) REFERENCES Administrator(AID)
+);
 
-create table Tag(
-	DID int(),
-	TagName varchar() unique,
-	UID int(),
-	CreationDate date(),#pas besoin du time 
-	Modified bool(),
-    primary key(DID),
-	foreign key (UID) references BookUser(UID)     
-)
+CREATE TABLE DescriberDeletion(
+	DID INT,
+	UID INT,
+	DeletionDate DATETIME NOT NULL,
+    FOREIGN KEY (DID) REFERENCES Describer(DID),
+	FOREIGN KEY (UID) REFERENCES BookUser(UID)
+);
 
-create table UserSignal(
-	DID int(),
-	SignalerUID int()
- 	foreign key (DID) references BookComment(DID),    
-	foreign key (SignalerUID) references Administrator(AID)       
-)
-
-create table TagDescribe(
-	TagName varchar(),
-	EID int(),
-	UID int()
-	foreign key (EID) references Establishment(EID),     
-	foreign key (AID) references Administrator(AID)    
-)
-
-
-
-	
+CREATE TABLE DescriberModification(
+	OldDID INT,
+	NewDID INT,
+	UID INT,
+	ModificationDate DATETIME NOT NULL,
+	FOREIGN KEY (OldDID) REFERENCES Describer(DID),
+	FOREIGN KEY (NewDID) REFERENCES Describer(DID),
+	FOREIGN KEY (UID) REFERENCES BookUser(UID)
+);
 	
