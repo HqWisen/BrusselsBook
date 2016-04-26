ea4e20d5d06b9f8b1fb0c98f3e2dd8fbff1c4927
Hakim Boulahya <hakimblhya@gmail.com>
2016-04-27 00:07:07 +0200

    first src files

--- src/be/brusselsbook/servs/Home.java
+++ src/be/brusselsbook/servs/Home.java
@@ -3,17 +3,19 @@ package be.brusselsbook.servs;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/home")
 public class Home extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
+		getServletContext().getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(req, resp);
 	}
 
 }
