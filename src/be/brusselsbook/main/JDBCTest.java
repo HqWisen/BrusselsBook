9959491d73d3e81274432fd53a4595320a67129d
Hakim Boulahya <hakimblhya@gmail.com>
2016-04-30 23:00:32 +0200

    DatabaseAccess with properties file

--- src/be/brusselsbook/main/JDBCTest.java
+++ src/be/brusselsbook/main/JDBCTest.java
@@ -12,20 +12,16 @@ public class JDBCTest {
 	private static final String QUERY = "select * from BookUser;";
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		
 		System.out.println("Running jdbc testing...");
 		DatabaseAccess databaseAccess = new DatabaseAccess();
 		Connection connection = databaseAccess.getConnection();
 		PreparedStatement statement = connection.prepareStatement(QUERY);
 		ResultSet resultSet = statement.executeQuery();
-		Class.forName("com.mysql.jdbc.Driver"); // chargement du driver JDBC
-	
 		while (resultSet.next()) {
 			String email = resultSet.getString("EmailAddress");
 			int UID = resultSet.getInt("UID");
 			System.out.println(email + " > " + UID);
 		}
-		
 		connection.close();
 		databaseAccess.close();
 	}
--- src/be/brusselsbook/sql/DatabaseAccess.java
+++ src/be/brusselsbook/sql/DatabaseAccess.java
@@ -1,50 +1,119 @@
 package be.brusselsbook.sql;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import com.jolbox.bonecp.BoneCP;
 import com.jolbox.bonecp.BoneCPConfig;
 
-public class DatabaseAccess extends BoneCP{
-	
-	private static final long serialVersionUID = 1373811783278087243L;
-	private static final String DRIVERCLASS = "com.mysql.jdbc.Driver";
-	private static final String DBURL = "jdbc:mysql://localhost:3306/brusselsbook";
-	private static final String DBUSER = "bbadmin";
-	private static final String DBPWD = "common";
-	private static final int MINCONNECTIONS = 5;
-	private static final int MAXCONNECTIONS = 10;
-	private static final int PARTITIONCOUNT = 2;
+import be.brusselsbook.sql.exception.AccessException;
 
-	static{
-		loadDriver();
-	}
+public class DatabaseAccess extends BoneCP {
+
+	private static final long serialVersionUID = 1373811783278087243L;
+	private static final String PROPRETIESFILE = "access.properties";
+	private static final DatabaseProperties DATABASEPROPERTIES;
 	
-	private static void loadDriver() {
+	private static class DatabaseProperties {
+		private final String driverClass;
+		private final String url;
+		private final String user;
+		private final String password;
+		private final int minConnection;
+		private final int maxConnection;
+		private final int partitionCount;
+
+		public DatabaseProperties(Properties properties) {
+			this.driverClass = properties.getProperty("driverClass");
+			this.url = properties.getProperty("url");
+			this.user = properties.getProperty("user");
+			this.password = properties.getProperty("password");
+			this.minConnection = Integer.parseInt(properties.getProperty("minConnection"));
+			this.maxConnection = Integer.parseInt(properties.getProperty("maxConnection"));
+			this.partitionCount = Integer.parseInt(properties.getProperty("partitionCount"));
+		}
+
+		public String getDriverClass() {
+			return driverClass;
+		}
+
+		public String getUrl() {
+			return url;
+		}
+
+		public String getUser() {
+			return user;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public int getMinConnection() {
+			return minConnection;
+		}
+
+		public int getMaxConnection() {
+			return maxConnection;
+		}
+
+		public int getPartitionCount() {
+			return partitionCount;
+		}
+
+	}
+
+	static {
+		DATABASEPROPERTIES = createDefaultProperties();
+		loadDriver(DATABASEPROPERTIES);
+	}
+
+	public static void loadDriver(DatabaseProperties properties) {
 		try {
-			Class.forName(DRIVERCLASS);
+			Class.forName(properties.getDriverClass());
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-		}	
+		}
 	}
-	
-	private static BoneCPConfig createDefaultConfig() {
+
+	private static DatabaseProperties createDefaultProperties() {
+		Properties properties = new Properties();
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream fichierProperties = classLoader.getResourceAsStream(PROPRETIESFILE);
+		if (fichierProperties == null) {
+			throw new AccessException("the properties file '" + PROPRETIESFILE + "' cannot be found.");
+		}
+		try {
+			properties.load(fichierProperties);
+		} catch (IOException e) {
+			throw new AccessException("Unable to load the properties file '" + PROPRETIESFILE + "'.");
+		}
+		return new DatabaseProperties(properties);
+	}
+
+	private static BoneCPConfig createDefaultConfig(DatabaseProperties properties) {
 		BoneCPConfig config = new BoneCPConfig();
-		config.setJdbcUrl(DBURL);
-		config.setUsername(DBUSER);
-		config.setPassword(DBPWD);
-		config.setMinConnectionsPerPartition(MINCONNECTIONS);
-		config.setMaxConnectionsPerPartition(MAXCONNECTIONS);
-		config.setPartitionCount(PARTITIONCOUNT);
+		config.setJdbcUrl(properties.getUrl());
+		config.setUsername(properties.getUser());
+		config.setPassword(properties.getPassword());
+		config.setMinConnectionsPerPartition(properties.getMinConnection());
+		config.setMaxConnectionsPerPartition(properties.getMaxConnection());
+		config.setPartitionCount(properties.getPartitionCount());
 		return config;
 	}
-	
-	public DatabaseAccess() throws SQLException{
-		this(createDefaultConfig());
+
+	public DatabaseAccess() throws SQLException {
+		this(DATABASEPROPERTIES);
+	}
+
+	public DatabaseAccess(DatabaseProperties properties) throws SQLException {
+		this(createDefaultConfig(properties));
 	}
 
 	private DatabaseAccess(BoneCPConfig config) throws SQLException {
 		super(config);
 	}
-	
+
 }
