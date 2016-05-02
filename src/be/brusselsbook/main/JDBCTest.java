e52d0904932a359a4bb017461b0a898b51bbc889
BOUHARAOUA Youcef <ybouhara@ulb.ac.be>
2016-05-02 12:27:39 +0200

    ask to push

--- src/be/brusselsbook/main/JDBCTest.java
+++ src/be/brusselsbook/main/JDBCTest.java
@@ -20,7 +20,7 @@ public class JDBCTest {
 		EstablishmentAccess<Establishment> establishmentAccess = factory.getEstablishmentAccess();
 		AddressAccess addressAccess = factory.getAddressAccess();
 		System.out.println(addressAccess.withEid(1L));
-		//System.out.println(establishmentAccess.createEstablishment("Jimmy Burger", "02488371627", "http://jimmyburger.be"));
+		System.out.println(establishmentAccess.createEstablishment("Jimmy Burger", "02488371627", "http://jimmyburger.be"));
 		//System.out.println(addressAccess.createAddress(1L, "Rue steens", "7", "Saint-Gilles", "1060", 4.4567f, 50.65498f));
 		/*System.out.println(bookUserAccess.SELECTBY("UID"));
 		System.out.println(administratorAccess.withUsername("jordan"));
