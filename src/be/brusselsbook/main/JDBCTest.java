31ac29cb586c96276d1c02531bbb021f7b8907fe
BOUHARAOUA Youcef <ybouhara@ulb.ac.be>
2016-05-02 16:58:30 +0200

    git conflit fixed

--- src/be/brusselsbook/main/JDBCTest.java
+++ src/be/brusselsbook/main/JDBCTest.java
@@ -32,31 +32,6 @@ public class JDBCTest {
 		for(CafeXml cx : cafeList){
 			cafeAccess.createCafe(cx.getCafeInfos());
 		}
-		/*AdministratorAccess administratorAccess = factory.getAdminstratorAccess();
-		BookUserAccess<BookUser> bookUserAccess = factory.getBookUserAccess();
-		EstablishmentAccess<Establishment> establishmentAccess = factory.getEstablishmentAccess();
-		AddressAccess addressAccess = factory.getAddressAccess();
-		System.out.println(addressAccess.withEid(1L));
-		System.out.println(establishmentAccess.createEstablishment("Jimmy Burger", "02488371627", "http://jimmyburger.be"));
-<<<<<<< HEAD
-		//System.out.println(addressAccess.createAddress(1L, "Rue steens", "7", "Saint-Gilles", "1060", 4.4567f, 50.65498f));
-		/*System.out.println(bookUserAccess.SELECTBY("UID"));
-=======
-		System.out.println(addressAccess.createAddress(1L, "Rue steens", "7", "Saint-Gilles", "1060", 4.4567f, 50.65498f));
-		System.out.println(bookUserAccess.SELECTBY("UID"));
->>>>>>> 719edb514a464393a7d8453396449c87b6f401df
-		System.out.println(administratorAccess.withUsername("jordan"));
-		System.out.println(administratorAccess.withUsername("kevin"));
-		System.out.println(administratorAccess.withEmail("jimmyjam@brusselsbook.be"));
-		System.out.println(administratorAccess.withUid("3"));
-		System.out.println(administratorAccess.withAid("1"));
-		System.out.println(bookUserAccess.withEmail("kevinspacey@brusselsbook.be"));
-		System.out.println(bookUserAccess.withUsername("kevin"));
-		System.out.println(bookUserAccess.withUid(1L));
-		establishmentAccess.create("Jimmy Burger Burger", "02322222", "http://jimmyburger.com");
-		System.out.println(establishmentAccess.getObjects());
-		BookUser bookUser = administratorAccess.create("hakimaa@brusselsbook.be", "hakimaa", "nous");
-		System.out.println(bookUser);*/	
 	}
 	
 	
